bb9f05cb3e4169cfd8e8a205736d1c36a6b36e92
yeezy-na-izi <howone1sky@gmail.com>
2022-09-19 12:12:10 +0300

    Make dz

--- 19.09/dz.c
+++ 19.09/dz.c
@@ -1,18 +1,15 @@
-// make algorithm for sqrt of number, number = float
+// make algorithm for sqrt of number, number = float number get from cmd
 
 
 #include <stdio.h>
+#include <stdlib.h>
 
 
-int main(int argc, char **argv) {
-    float number, result = 1;
-    printf("Enter number: ");
-    scanf("%f", &number);
-
+int main(int argc, char *argv[]) {
+    float number = atof(argv[1]);
+    float result = 1;
     for (int i = 0; i < 100; i++) {
         result = (result + number / result) / 2;
     }
-
-    printf("Result: %f\n", result);
-    return 0;
+    printf("Result %f", result);
 }
