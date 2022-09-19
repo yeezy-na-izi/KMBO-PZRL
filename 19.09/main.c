1ea333bec8c05ee91eca25ffa83f0768502f1772
yeezy-na-izi <howone1sky@gmail.com>
2022-09-19 11:16:47 +0300

    Change something in main.c

---
+++ 19.09/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 0;
+    int b = 0;
+
+    printf("Enter a: ");
+    scanf("%d", &a);
+    printf("Enter b: ");
+    scanf("%d", &b);
+
+    printf("a + b = %d", a + b);
+    printf("Hello, world!")
+}
