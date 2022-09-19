2b162273ca96caab1f5d8d211a33eb35985756de
yeezy-na-izi <howone1sky@gmail.com>
2022-09-19 13:06:05 +0300

    cosmetics changes

--- 19.09/dz.c
+++ 19.09/dz.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < 100; i++) {
         result = (result + number / result) / 2;
     }
-    printf("Result %f", result);
+    printf("Result %f\n", result);
 
     return 0;
 }
