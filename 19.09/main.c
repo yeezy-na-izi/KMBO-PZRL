43c676425f113eefd64dde2ab6b9067ea3c9643d
yeezy-na-izi <howone1sky@gmail.com>
2022-09-19 11:27:56 +0300

    Make pow algorithm

--- 19.09/main.c
+++ 19.09/main.c
@@ -1,14 +1,20 @@
+// make algorithm pow of number
+
+
 #include <stdio.h>
 
-int main(void) {
-    int a = 0;
-    int b = 0;
 
-    printf("Enter a: ");
-    scanf("%d", &a);
-    printf("Enter b: ");
-    scanf("%d", &b);
+int main(int argc, char **argv) {
+    int number, pow, result = 1;
+    printf("Enter number: ");
+    scanf("%d", &number);
+    printf("Enter pow: ");
+    scanf("%d", &pow);
 
-    printf("a + b = %d", a + b);
-    printf("Hello, world!")
+    for (int i = 0; i < pow; i++) {
+        result *= number;
+    }
+
+    printf("Result: %d\n", result);
+    return 0;
 }
