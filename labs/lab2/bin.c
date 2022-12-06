abbef80d76922c0175a9f155bca6c1f7b0275264
yeezy-na-izi <howone1sky@gmail.com>
2022-12-06 19:15:01 +0300

    Add from_bin and to_bin funcs

--- labs/lab2/bin.c
+++ labs/lab2/bin.c
@@ -1,1 +1,30 @@
 #include "bin.h"
+#include <string.h>
+#include <stdlib.h>
+
+int from_bin(char *number) {
+    int result = 0;
+    for (int i = 2; i < strlen(number); i++) {
+        if (number[i] >= '0' && number[i] <= '1') {
+            result = result * 2 + (number[i] - '0');
+        }
+    }
+    return result;
+}
+
+char *to_bin(int number) {
+    char *result = (char *) malloc(sizeof(char));
+    while (number > 0) {
+        int digit = number % 2;
+        result = (char *) realloc(result, (strlen(result) + 1) * sizeof(char));
+        result[strlen(result)] = digit + '0';
+        number /= 2;
+    }
+    char *result_reversed = (char *) malloc(sizeof(char));
+    for (int i = strlen(result) - 1; i >= 0; i--) {
+        result_reversed = (char *) realloc(result_reversed, (strlen(result_reversed) + 1) * sizeof(char));
+        result_reversed[strlen(result_reversed)] = result[i];
+    }
+    free(result);
+    return result_reversed;
+}
--- labs/lab2/bin.h
+++ labs/lab2/bin.h
@@ -1,4 +1,8 @@
 #ifndef PZRL_BIN_H
 #define PZRL_BIN_H
 
+int from_bin(char *number);
+
+char *to_bin(int number);
+
 #endif //PZRL_BIN_H
